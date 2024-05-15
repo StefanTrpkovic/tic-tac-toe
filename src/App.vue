4936dcf6fcd3400ade342eca6db67a873dfc7c36
Stefan Trpkovic <stefan.trpkovik@gmail.com>
2024-05-15 10:43:52 +0200

    Better logic

--- src/App.vue
+++ src/App.vue
@@ -1,128 +1,129 @@
 <script setup>
-import { ref, computed } from "vue"
+import { ref, computed } from "vue";
 
-const player = ref("X")
-// const board = ref([
-//   ["0", "1", "2"],
-//   ["3", "4", "5"],
-//   ["6", "7", "8"],
-// ])
-
+const player = ref("X");
 const board = ref([
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
-])
+]);
 
-const isHorizontalWin = computed(() => {
+const checkRowForSame = (row) => {
+  if (row.filter((i) => i !== "").length) {
+    const threeInRow = row.every((i) => i === row[0]);
+    if (threeInRow) return row[0];
+  }
+
+  return false;
+};
+
+const threeInRowHorizontally = computed(() => {
   for (let i = 0; i < board.value.length; i++) {
-    const row = board.value[i]
-    const zeroCell = row[0]
-
-    if (row.every((cell) => cell === zeroCell)) {
-      return zeroCell
+    if (checkRowForSame(board.value[i])) {
+      return board.value[i][0];
     }
   }
 
-  return false
-})
+  return false;
+});
 
-const isVerticalWin = computed(() => {
-  const firstColumn = [],
-    secondColumn = [],
-    thirdColumn = []
+const threeInRowVertically = computed(() => {
+  const columns = board.value.reduce(
+    (result, row) =>
+      row.map((value, index) => (result[index] || []).concat(value)),
+    []
+  );
 
-  for (let i = 0; i < board.value.length; i++) {
-    const row = board.value[i]
-    firstColumn.push(row[0])
-    secondColumn.push(row[1])
-    thirdColumn.push(row[2])
+  for (let i = 0; i < columns.length; i++) {
+    if (checkRowForSame(columns[i])) {
+      return columns[i][0];
+    }
   }
+  console.log(columns);
+});
 
-  if (firstColumn.every((cell) => cell === firstColumn[0] && cell !== ""))
-    return firstColumn[0]
-  if (secondColumn.every((cell) => cell === secondColumn[0] && cell !== ""))
-    return secondColumn[0]
-  if (thirdColumn.every((cell) => cell === thirdColumn[0] && cell !== ""))
-    return thirdColumn[0]
+const threeInRowDiagonally = computed(() => {
+  const leftToRight = checkRowForSame([
+    board.value[0][0],
+    board.value[1][1],
+    board.value[2][2],
+  ]);
 
-  return false
-})
+  if (leftToRight) return leftToRight;
 
-const isDiagonalWin = computed(() => {
-  const firstDiagonal = [],
-    secondDiagonal = []
+  const rightToLeft = checkRowForSame([
+    board.value[0][2],
+    board.value[1][1],
+    board.value[2][0],
+  ]);
 
-  firstDiagonal.push(board.value[0][0], board.value[1][1], board.value[2][2])
-  secondDiagonal.push(board.value[0][2], board.value[1][1], board.value[2][0])
+  if (rightToLeft) return rightToLeft;
 
-  if (firstDiagonal.every((cell) => cell === firstDiagonal[0] && cell !== ""))
-    return firstDiagonal[0]
-
-  if (secondDiagonal.every((cell) => cell === secondDiagonal[0] && cell !== ""))
-    return secondDiagonal[0]
-
-  return false
-})
+  return false;
+});
 
 const winner = computed(() => {
-  return isHorizontalWin.value || isVerticalWin.value || isDiagonalWin.value
-})
+  return (
+    threeInRowHorizontally.value ||
+    threeInRowVertically.value ||
+    threeInRowDiagonally.value ||
+    null
+  );
+});
+
+const move = (x, y) => {
+  if (winner.value || board.value[x][y] !== "") return;
+  board.value[x][y] = player.value;
+  player.value = player.value === "X" ? "0" : "X";
+};
+
+const isOver = computed(() => winner.value || isTie.value);
 
 const isTie = computed(() => {
-  let result = true
-  for (let i = 0; i < board.value.length; i++) {
-    const row = board.value[i]
-    for (let i = 0; i < row.length; i++) {
-      const cell = row[i]
-      if (cell === "") {
-        result = false
-        break
-      }
-    }
-  }
-
-  return result
-})
-
-const onCellClick = (x, y) => {
-  if (player.value === "X") {
-    board.value[x][y] = "X"
-    player.value = "0"
-  } else {
-    board.value[x][y] = "0"
-    player.value = "X"
-  }
-}
+  console.log(board.value.flat);
+  return !board.value.flat().includes("") && !winner.value;
+});
 
 const reset = () => {
   board.value = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
-  ]
-}
+  ];
+
+  player.value = "X";
+};
 </script>
 
 <template>
-  <main class="h-screen flex flex-col justify-center text-center">
-    <div class="mb-4">
-      <h1 class="text-3xl font-bold">Tic Tac Toe</h1>
-      <p v-if="winner">
-        Player {{ winner === "X" ? 1 : winner === "0" ? 2 : "" }} wins
-      </p>
-      <p v-else-if="isTie">It's a tie</p>
-      <button @click="reset">Start new game</button>
-    </div>
-    <div v-for="(row, x) in board">
+  <main class="pt-16 text-center">
+    <h1 class="mb-3 text-3xl font-bold">Tic-Tac-Toe</h1>
+    <h3 v-if="!isOver" class="mb-4 text-xl">Player {{ player }}'s turn</h3>
+    <div v-else>
+      <h3 v-if="winner" class="mb-8 text-6xl font-bold">Player '{{ winner }}' wins!</h3>
+      <h3 v-if="isTie" class="mb-8 text-6xl font-bold">Cat got it!</h3>
       <button
-        v-for="(cell, y) in row"
-        class="border border-white p-10"
-        @click="onCellClick(x, y)"
-        :disabled="winner || isTie || cell !== ''"
+        @click="reset"
+        class="inline-block p-3 px-2 mb-8 bg-blue-700 rounded"
       >
-        {{ cell }}
+        New Game
       </button>
+    </div>
+    <div class="flex flex-col items-center">
+      <div v-for="(row, x) in board" :key="x" class="flex">
+        <button
+          v-for="(cell, y) in row"
+          :key="y"
+          class="w-24 h-24 text-5xl border border-white"
+          :class="{
+            'text-lime-600': cell === 'X',
+            'text-orange-500': cell === '0',
+          }"
+          @click="move(x, y)"
+        >
+          {{ cell }}
+        </button>
+      </div>
     </div>
   </main>
 </template>
